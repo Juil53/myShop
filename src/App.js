160a458e3ac489ed37afcfae28f880c647174f72
Lê Ngọc Minh <ngocminh.2126@gmail.com>
2022-05-09 12:28:20 +0700

    merge code

--- src/App.js
+++ src/App.js
@@ -3,18 +3,21 @@ import { theme } from "./styles/muiStyles";
 import { ThemeProvider } from "@mui/material/styles";
 import HomePage from "./pages/User/HomePage/HomePage";
 import { Routes, Route } from "react-router-dom";
+import routeAdmin from "./routes/AdminRoute";
 import "./App.css";
 import { CssBaseline, Typography } from "@mui/material";
 // import Product from "./components/product";
-
+const routesAdmin = routeAdmin.map((route, index) => (
+  <Route key={index} path={route.path} element={route.element} />
+));
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <Routes>
-        <Route path="" element={<HomePage />} />
+        <Route path="/" element={<HomePage />} />
+        {routesAdmin}
       </Routes>
-      <AdminPage />
     </ThemeProvider>
   );
 }
