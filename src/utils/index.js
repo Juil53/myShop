6e932f6894d441a41ff7639159a609fbccac0b76
Lê Ngọc Minh <ngocminh.2126@gmail.com>
2022-06-08 23:23:10 +0700

    ngocminh | signin signup page

--- src/utils/index.js
+++ src/utils/index.js
@@ -62,6 +62,19 @@ export const removeAccents = (str) => {
   return str;
 };
 
+export const checkEmailFormat = (email) => {
+  const txt = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+  return txt.test(email);
+};
+
+//(123) 456-7890 (123)456-7890 123-456-7890 123.456.7890 1234567890 +31636363634 075-63546725
+export const checkPhoneFormat = (phone) => {
+  const txt = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+
+  return txt.test(phone);
+};
+
 export const debounce = (fn, time) => {
   let timer = null;
 
