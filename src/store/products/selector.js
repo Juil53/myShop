dabad34a796b7adfb960ff3efb8007cf70e8f30b
dvhNghia <dinhvanhuunghia@gmail.com>
2022-06-17 08:49:45 +0700

    dvhnghia | update selector, related products

--- src/store/products/selector.js
+++ src/store/products/selector.js
@@ -61,9 +61,7 @@ export const selectProduct = (state, mainCate, subCate, sortCate) => {
 export const selectProductInfo = (state, id) => {
   const { products = {} } = state || {};
   const { allProducts = {} } = products;
-  console.log("allproduct",allProducts)
   const { data = [] } = allProducts;
-
   const productSelected = data.find((product) => product.id == id);
   return productSelected;
 };
