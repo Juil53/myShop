2e9d3f75d9c9c9b5c53874d0a887647826e6d461
dvhNghia <dinhvanhuunghia@gmail.com>
2022-07-28 14:05:52 +0700

    dvhnghia | src/pages/user | fix searchBar, save query search in searchKey state

--- src/pages/user/product_detail/ProductDetail.jsx
+++ src/pages/user/product_detail/ProductDetail.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
+import { useParams, useSearchParams } from "react-router-dom";
 import {
   getQuantityAvailable,
   isAvailableOption,
